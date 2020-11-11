dfc6e8acdc09d30fab0cf1504747c728db2fa88a
wadkan <zsoltgaramszegi@gmail.com>
2020-11-11 21:01:32 +0100

    Juniper done

--- src/main/java/Juniper.java
+++ src/main/java/Juniper.java
@@ -1,2 +1,32 @@
-public class Juniper {
+public class Juniper extends Plant implements Evergreen {
+    int everGreenMonth;
+
+    public Juniper() {
+        super(14);
+    }
+
+    @Override
+    public void incrementMonthByOne() {
+        if (checkProducePerMonth()) {
+            super.incrementMonthByOne();
+            if (increaseGreenMonthByOn()) {
+                everGreenMonth = 0;
+                setProducePerMonth(getProducePerMonth() + 8);
+            }
+        }
+    }
+
+    public boolean increaseGreenMonthByOn() {
+        everGreenMonth++;
+        return (everGreenMonth == 5);
+    }
+
+    public boolean checkProducePerMonth() {
+        if (70 <= getProducePerMonth()) {
+            System.out.println("Juniper at full production!");
+            return false;
+        }
+        return true;
+
+    }
 }
