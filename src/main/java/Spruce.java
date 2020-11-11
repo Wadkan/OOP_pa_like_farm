5acde52d270ffd5955333a599894f53fdd3dc078
wadkan <zsoltgaramszegi@gmail.com>
2020-11-11 20:59:46 +0100

    Spruce done

--- src/main/java/Spruce.java
+++ src/main/java/Spruce.java
@@ -1,5 +1,33 @@
-public class Spruce extends GrowShroom {
+public class Spruce extends GrowShroom implements Evergreen {
+    int everGreenMonth;
+
     public Spruce() {
         super(26, 5, 15, 4);
+        this.everGreenMonth = 0;
+    }
+
+    @Override
+    public void incrementMonthByOne() {
+        if (checkProducePerMonth()) {
+            super.incrementMonthByOne();
+            if (increaseGreenMonthByOn()) {
+                everGreenMonth = 0;
+                setProducePerMonth(getProducePerMonth() + 8);
+            }
+        }
+    }
+
+    public boolean increaseGreenMonthByOn() {
+        everGreenMonth++;
+        return (everGreenMonth == 5);
+    }
+
+    public boolean checkProducePerMonth() {
+        if (70 <= getProducePerMonth()) {
+            System.out.println("Spruce at full production!");
+            return false;
+        }
+        return true;
+
     }
 }
